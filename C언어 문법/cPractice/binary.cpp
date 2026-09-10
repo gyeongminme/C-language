3f1cb805be787a3d50d3c0ce0c81943753c6d905
조경민 <qwsafdre@naver.com>
2026-09-10 19:58:14 +0900

    2진수 변환 함수

--- C언어 문법/cPractice/binary.cpp
+++ C언어 문법/cPractice/binary.cpp
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-void binary(int v) {
-    if (v > 1) {
-        binary(v >> 1);
-       
-    } //0000 0000
-    
-     printf("%d", v & 1);
-}
+//void binary(int v) {
+//    if (v > 1) {
+//        binary(v >> 1);
+//       
+//    }
+//    
+//     printf("%d", v & 1);
+//}
 
-void binary16(int v) {
+void binary2(int v) {
   
     for (int i = 15; i >= 0; i--) {
         int tmp = (v >> i);
@@ -23,9 +23,10 @@ void binary16(int v) {
 
 int main(void) {
 
-    printf("16진수");
+    printf("2진수 변환");
 
-    binary16(12);
+
+    binary2(12);
     
 
 
